58c6d7752bce0dfaf90d32ace93edf733ae63b29
shahi5472 <s.m.kamalhussain@gmail.com>
2026-05-12 08:07:05 +0600

    implement internet connectivity monitoring in App widget
    - Convert `App` from `StatelessWidget` to `StatefulWidget` to manage connectivity state
    - Integrate `InternetChangeNotifier` to track real-time connection status
    - Implement a listener to display status snackbars when internet is lost or restored
    - Add lifecycle management for the connectivity listener using `initState` and `dispose`

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,50 @@
 import 'dart:ui';
 
 import 'package:coaching_module_test/routes/app_pages.dart';
+import 'package:coaching_module_test/utils/app_utils.dart';
 import 'package:coaching_module_test/utils/constansts/global.dart';
 import 'package:coaching_module_test/utils/constansts/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:toastification/toastification.dart';
 
 import 'flavors.dart';
+import 'utils/dependency_injection/custom_getit.dart';
+import 'utils/manager/network/internet_change_notifier.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool? _previousConnectionState;
+  final InternetChangeNotifier _internetChangeNotifier = sl<InternetChangeNotifier>();
+
+  @override
+  void initState() {
+    _internetChangeNotifier.addListener(_internetListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _internetChangeNotifier.removeListener(_internetListener);
+    super.dispose();
+  }
+
+  void _internetListener() {
+    final bool hasInternet = _internetChangeNotifier.value;
+
+    if (_previousConnectionState == hasInternet) return;
+
+    _previousConnectionState = hasInternet;
+
+    final hasInternest = _internetChangeNotifier.value;
+    final text = hasInternest ? "Internet connection restored" : "No internet connection";
+    AppUtils.showSnackBar(context, !hasInternest, text);
+  }
 
   @override
   Widget build(BuildContext context) {
